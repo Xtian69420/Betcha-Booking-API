f2e6edf17b91725e85942f82b600711458569514
Xtian69420 <signo.303167@fairview.sti.edu.ph>
2025-06-30 04:49:07 +0800

    Update authenticationController.js

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -79,3 +79,5 @@ exports.updatePassword = async (req, res) => {
     res.status(500).json({ message: 'Internal server error.' });
   }
 };
+
+// https://in.sumsub.com/websdk/p/sbx_uni_5lWlhioi8FNABcxg
